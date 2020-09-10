5d30b004817f63cd20e1da05480a8b3b492b3979
solar <jarabelmonte@gmail.com>
2020-09-10 12:10:16 +0200

    undoing..

--- src/api/FetchWeather.js
+++ src/api/FetchWeather.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
-const API_KEY = '42f43db229167bf2380b0111e93868ca';
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 export const FetchWeather = async (query) => {
     const {data} = await axios.get(URL, {
